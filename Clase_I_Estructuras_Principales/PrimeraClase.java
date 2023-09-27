4df1a0496a9d852408e91e8c750624fe3099f48e
David Bustos Usta <dfbustos@uninorte.edu.co>
2023-09-27 11:05:00 -0300

    27-09-2023ClaseI

---
+++ Clase_I_Estructuras_Principales/PrimeraClase.java
@@ -0,0 +1,7 @@
+package Clase_I_Estructuras_Principales;
+
+public class PrimeraClase {
+    public static void main(String args[]){
+        System.out.println("Hola esta es la primera clase");
+    }
+}
---
+++ HelloApp/Hello.java
@@ -0,0 +1,12 @@
+package HelloApp;
+
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hola soy David");
+    }
+
+}
