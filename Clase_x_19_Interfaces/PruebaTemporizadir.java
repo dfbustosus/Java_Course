23d01d868a0dd4539144cf1423e017d6af6efa50
David Bustos Usta <dfbustos@uninorte.edu.co>
2023-10-13 08:29:33 -0300

    2023-10-13

---
+++ Clase_x_19_Interfaces/PruebaTemporizadir.java
@@ -0,0 +1,25 @@
+package Clase_x_19_Interfaces;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class PruebaTemporizadir {
+    public static void main(String[] args) {
+        darHora oyente = new darHora();
+        Timer miTemporizador = new Timer(5000, oyente);
+        miTemporizador.start();
+
+        JOptionPane.showMessageDialog(null, "Pulsa aceptar para detener");
+        System.exit(0);
+    }
+}
+
+class darHora implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        Date ahora = new Date();
+        System.out.println("La hora cada 5 segundos: " + ahora);
+    }
+}
+
