30134fed4704478e27502bc777cdbce75ffecd35
David Bustos Usta <dfbustos@uninorte.edu.co>
2023-10-14 19:31:02 -0300

    14-10-2023v1

---
+++ Clase_x_20_Clases_Internas/Clases_internas.java
@@ -0,0 +1,43 @@
+package Clase_x_20_Clases_Internas;
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.Toolkit;
+
+public class Clases_internas {
+    public static void main(String[] args){
+        Reloj mireloj= new Reloj(5000, true);
+        mireloj.enmarcha();
+        JOptionPane.showMessageDialog(null, "Pulsa Aceptar para terminar");
+        System.exit((0));
+
+    }
+}
+    
+class Reloj{
+    private int intervalo;
+    private boolean sonido;
+    // Constructor
+    public Reloj(int intervalo, boolean sonido){
+        this.intervalo=intervalo;
+        this.sonido= sonido;
+    }
+    public void enmarcha(){
+        // Clase interna
+        class darHora implements ActionListener{
+            public void actionPerformed(ActionEvent evento){
+                Date ahora = new Date();
+                System.out.println("La hora cada 5 seg");
+                if(sonido ==true){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        }
+        
+        ActionListener oyente= new darHora();
+        Timer mitemporizador= new Timer(intervalo,oyente);
+        mitemporizador.start();
+    }
+
+}
--- Clase_x_20_Clases_Internas/Teoria.md
+++ Clase_x_20_Clases_Internas/Teoria.md
@@ -18,3 +18,29 @@ public class Clase 1 {
 - Para crear clases internas **anonimas** muy utiles para gestionar eventos y retrollamadas
 - Cuando una clase debe acceder a los campos de ejemplar de otra clase
 
+## Clases internas locales
+
+- Son unca clase dentro de un metódo
+
+**¿Cuando las usamos y porque?**
+
+- Cuando solo se va a utilizar (instanciar) la clase interna una vez. El objetivo es simplificar el código
+- Su ambito queda restringido al mtódo donde son declaradas
+
+**Ventajas**
+
+- Estas muy *encapsuladas*. Ni siquiera la clase a la que pertenecen pueden acceder a ella. Tan solo puede acceder a ella el metódo donde están declaradas
+- El código resulta más simple
+
+```java
+class Clase_externa{
+    public void metodo(){
+        class clase_interna_local{
+            codigo_clase_interna
+        }
+    codigo_metodo
+    }
+codigo_clase_externa
+}
+```
+
